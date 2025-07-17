bfd32fd99face05c0b227785b46c989e6bff607b
Zaiulani Zantidi <zai.jacks@live.com>
2025-07-17 00:56:22 +0000

    ZaiulaniZantidi -Conditional rendering Profile Card social media link revision.

--- src/js/app.js
+++ src/js/app.js
@@ -38,10 +38,10 @@ function render(variables = {}) {
           <h3 class = "picker">${variables.city}</h3>
           <h3 class = "picker">${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
